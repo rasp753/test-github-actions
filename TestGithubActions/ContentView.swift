b5ddd0ce5b20becdd387926e4d8344e46139697a
Masato Tomita <21194634+rasp753@users.noreply.github.com>
2023-07-18 17:16:14 +0900

    Initial commit.

---
+++ TestGithubActions/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TestGithubActions
+//
+//  Created by 富田 雅斗 on 2023/07/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestGithubActions/TestGithubActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGithubActionsApp.swift
+//  TestGithubActions
+//
+//  Created by 富田 雅斗 on 2023/07/18.
+//
+
+import SwiftUI
+
+@main
+struct TestGithubActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
